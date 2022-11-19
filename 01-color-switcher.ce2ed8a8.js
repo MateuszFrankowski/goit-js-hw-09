e2b2735cfe7e65cd94f22e09bff82524ebb1084d
MateuszFrankowski <106771721+MateuszFrankowski@users.noreply.github.com>
2022-11-19 20:27:59 +0000

    Deploying to gh-pages from @ MateuszFrankowski/goit-js-hw-09@bac78e41831067941bebdf67504220385bf6f304 🚀

---
+++ 01-color-switcher.ce2ed8a8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]"),l=()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0,d.disabled=!1};let o=null;t.addEventListener("click",(()=>o=setInterval(l,1e3))),d.addEventListener("click",(()=>{clearInterval(o),t.disabled=!1,d.disabled=!0,console.log(`Interval with id ${o} has stopped!`)})),d.disabled=!0;
+//# sourceMappingURL=01-color-switcher.ce2ed8a8.js.map
