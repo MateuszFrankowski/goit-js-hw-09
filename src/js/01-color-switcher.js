bac78e41831067941bebdf67504220385bf6f304
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2022-11-19 21:27:31 +0100

    task 1 done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+const bodyColorChanger = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+};
+let timerId = null;
+const bodyColorfunction = () => (timerId = setInterval(bodyColorChanger, 1000));
+const clearColorFunction = () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  console.log(`Interval with id ${timerId} has stopped!`);
+};
+
+btnStart.addEventListener('click', bodyColorfunction);
+btnStop.addEventListener('click', clearColorFunction);
+btnStop.disabled = true;
