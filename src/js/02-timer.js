1a806852b3ef222762ecd7bf09239d065572826c
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2022-11-20 20:14:08 +0100

    timmer updated

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+// Opisany w dokumentacji
+import flatpickr from 'flatpickr';
+// Dodatkowy import stylów
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr('#datetime-picker', options);
